6417bacc3b7dd7d6f4622925a340e30eea08ce14
Jakob Jensen <jakob.jensen.91@gmail.com>
2013-10-08 20:39:21 +0200

    Added TemplateNode

---
+++ g5/car_software/TemplateNode/init.h
@@ -0,0 +1,14 @@
+// OI
+void ioinit(void);
+
+// ADC
+void adcInit(unsigned int);
+
+// PWM functions
+void pwm16Init2(void);
+
+// UART
+void uartinit(void);
+void sendtekst(char*);
+
+void counter0Init(void);
