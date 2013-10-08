9b2935dae2ff2439db0ce2cfc61cc64d4131f386
Jakob Jensen <jakob.jensen.91@gmail.com>
2013-10-08 23:01:34 +0200

    restructuring templateNode added functions.h

---
+++ g5/car_software/TemplateNode/functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void gearNeutral1(void);
+void gearNeutral2(void);
+void sendtekst(char *tekstarray);
+void gearUp(void);
+void gearDown(void);
+
+#endif /* FUNCTIONS_H */
