dc69d41920a4c452e36a205bfe80a66c2475a357
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2013-10-17 19:48:45 +0200

    added set prescalar function

--- g5/car_software/TemplateNode/adc.h
+++ g5/car_software/TemplateNode/adc.h
@@ -1,6 +1,14 @@
 #ifndef ADC_H
 #define ADC_H
 
-
+enum prescalar_t {
+	PRESCALAR_2 = 2,
+	PRESCALAR_4 = 4,
+	PRESCALAR_8 = 8,
+	PRESCALAR_16 = 16,
+	PRESCALAR_32 = 32,
+	PRESCALAR_64 = 64,
+	PRESCALAR_128 = 128
+};
 
 #endif /* ADC_H */
