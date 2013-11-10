e18002790b0431defad91aa3c81e36f149f5fd54
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2013-11-10 17:25:00 +0100

    implemented simpler can init code

--- g5/car_software/TemplateNode/main.c
+++ g5/car_software/TemplateNode/main.c
@@ -19,28 +19,19 @@ int main(void)
 {
 	//Initialise the Gear node
 	ioinit();									//Port setup
-	uart_init(UART_NUMBER_1);								//Serial communication
-	CAN_INIT();								//Can setup
+	uart_init(UART_NUMBER_1);					//Serial communication
+	CAN_INIT_RX();								//Can setup
     pwm16Init2();								//Setup PWM controller
-	//adcInit(1);									//Setup ADC for pot-meter or Amp meter
 	counter0Init();								//Init interrupt counter to overflow with 168Hz
 
 	sei();										//Enable interrupt
-
-	//Enable all can interrupts
-	CAN_SEI();
-	CAN_RX_SEI();
 
 	adc_setChannel(1);
 	adc_setVref(INTERNAL);
 	adc_enable();
 	adc_setPrescaler(PRESCALAR_128);
 
-	uart_txstring(UART_NUMBER_1, "\r\n");
-	uart_txstring(UART_NUMBER_1, "\r\n");
-	uart_txstring(UART_NUMBER_1, "\r\n");
-
-	uart_txstring(UART_NUMBER_1, "STARTING \r\n");
+	uart_txstring(UART_NUMBER_1, "\r\n\r\n\r\nSTARTING \r\n");
 
 	int i=0;
 	while(1){
--- g5/car_software/libat90/can.h
+++ g5/car_software/libat90/can.h
@@ -3,14 +3,18 @@
 
 #include "can_std/can_lib.h"
 
-#define CAN_INIT()		(can_init(0))
 
-#define CAN_SEI()		(Can_sei())
-#define CAN_RX_SEI()	(Can_set_rx_int())
-#define CAN_TX_SEI()	(Can_set_tx_int())
+#define CAN_SEI()		( Can_sei()			)
+#define CAN_RX_SEI()	( Can_set_rx_int() 	)
+#define CAN_TX_SEI()	( Can_set_tx_int() 	)
+
+#define CAN_INIT()		( can_init(0) )
+#define CAN_INIT_ALL()	{ CAN_INIT(); CAN_SEI(); CAN_RX_SEI(); CAN_TX_SEI(); 	}
+#define CAN_INIT_RX()	{ CAN_INIT(); CAN_SEI(); CAN_RX_SEI(); 					}
+#define CAN_INIT_TX()	{ CAN_INIT(); CAN_SEI(); CAN_TX_SEI(); 					}
 
 #define CAN_FORCE_CMD(cmd)			while(can_cmd((cmd)) != CAN_CMD_ACCEPTED)
-#define CAN_FORCE_COMPLETE(cmd)		while( can_get_status((cmd)) != CAN_STATUS_COMPLETED)
+#define CAN_FORCE_COMPLETE(cmd)		while( can_get_status((cmd)) != CAN_STATUS_COMPLETED )
 
 void can_send(int id, uint8_t *data, size_t length);
 void can_receive(int id, uint8_t *data, size_t length);
