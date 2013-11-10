828edcce6f1b26ead72eab98d6c4553ca25073c8
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2013-11-10 17:00:03 +0100

    Now using the new can_receive and can_send function in our test

--- g5/car_software/libat90/can.c
+++ g5/car_software/libat90/can.c
@@ -11,7 +11,7 @@
 
 #define SIZEOF_ARR(arr) (sizeof(arr) / sizeof(arr[0]))
 
-#define TEST_MSG_ID		4
+#define TEST_MSG_ID		4 // Debuggin msg id
 #define TEST_MSG_LEN	5 // length of the test message used for debugging
 
 void can_send(int id, uint8_t *data, size_t length){
@@ -45,18 +45,7 @@ void can_receive(int id, uint8_t *data, size_t length){
 
 void canTestReceiver(void){
 	uint8_t received_data[TEST_MSG_LEN] = {};
-	st_cmd_t received_msg = {
-		.pt_data = &received_data[0],
-		.status = 0,
-
-		.id.std = TEST_MSG_ID,
-		.ctrl.ide = 0,
-		.ctrl.rtr = 0,
-		.dlc = TEST_MSG_LEN,
-		.cmd = CMD_RX_DATA_MASKED // CMD_RX_DATA_MASKED gives interrupt while CMD_RX_DATA does not
-	};
-	CAN_FORCE_CMD(&received_msg); // Rx cmd
-	CAN_FORCE_COMPLETE(&received_msg);
+	can_receive(TEST_MSG_ID, &received_data[0], TEST_MSG_LEN);
 
 	uart_txstring(UART_NUMBER_1, "CAN rev: ");
 	uart_txarr(UART_NUMBER_1, &received_data[0], TEST_MSG_LEN);
@@ -65,17 +54,7 @@ void canTestReceiver(void){
 
 void canTestSender(void){
 	uint8_t databuffer[TEST_MSG_LEN] = {'H', 'e', 'l', 'l', 'o'}; 
-
-	st_cmd_t msg = {
-		.pt_data = &databuffer[0],
-		.ctrl.ide = 0,
-		.dlc = TEST_MSG_LEN,
-		.id.std = TEST_MSG_ID,
-		.cmd = CMD_TX_DATA
-	};
-
-	CAN_FORCE_CMD(&msg);
-	CAN_FORCE_COMPLETE(&msg);
+	can_send(TEST_MSG_ID, &databuffer[0], TEST_MSG_LEN);
 }
 
 /* Interrupt routine to take care of can interrupts */
