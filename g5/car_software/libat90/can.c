1356cb928a5ee6c45d7eafd42946256b38f16a27
Jakob Jensen <jakob.jensen.91@gmail.com>
2013-11-24 04:05:07 +0100

    More code cleanup in can.c

--- g5/car_software/libat90/can.c
+++ g5/car_software/libat90/can.c
@@ -1,45 +1,16 @@
-
-#include <stdint.h>
 #include <stdio.h>
-#include <string.h>
-
-#include <avr/io.h>
 #include <avr/interrupt.h>
-
-#include "can_std/can_lib.h"
 #include "can.h"
 #include "uart.h"
 
-#include <util/delay.h>
-
-#define SIZEOF_ARR(arr) (sizeof(arr) / sizeof(arr[0]))
-
-#define TEST_MSG_ID		4 // Debuggin msg id
-#define TEST_MSG_LEN	5 // length of the test message used for debugging
-
-void can_send(int id, uint8_t *data, uint8_t length){
-	st_cmd_t msg = {
-		.pt_data = &data[0],
-		.id.std = id,
-
-		.ctrl.ide = 0, // ide : identifier extension: 0 = can2.0A (11bit), 1 = can2.0B (29bit)
-		//.ctrl.rtr = 0, // rtr : request remote transmission
-		
-		.dlc = length,
-		
-		.cmd = CMD_TX_DATA
-	};
-
-	CAN_FORCE_CMD(&msg);
-	CAN_FORCE_COMPLETE(&msg);
-}
+static uint8_t msg_buff[NB_DATA_MAX + 1] = {0};
 
 /*
  * The Can_clear_mob() function clears the following registers:
  * CANSTMOB 			-- Contains interrupt status
  * CANCDMOB 			-- Defines MOB mode and msg length
  * CANIDT1 ... CANIDT4	-- CAN Identifier Tag Registers
- * CANIDM1 ... CANIDT4 	-- CAN Identifier Mask Registers
+ * CANIDM1 ... CANIDT4	-- CAN Identifier Mask Registers
  */
 
 void mob_create(int mob_id, st_cmd_t* descriptor) {
@@ -55,7 +26,21 @@ void mob_create(int mob_id, st_cmd_t* descriptor) {
 	Can_set_mob_int(mob_id);					/* Enable interrupt for MOB */
 }
 
-void can_receive(int mob_id, int id, uint8_t *data, uint8_t length){
+void can_send(int id, uint8_t *data, uint8_t length) {
+	st_cmd_t msg = {
+		.pt_data = &data[0],
+		.id.std = id,
+		.ctrl.ide = 0, // ide : identifier extension: 0 = can2.0A (11bit), 1 = can2.0B (29bit)
+		//.ctrl.rtr = 0, // rtr : request remote transmission
+		.dlc = length,
+		.cmd = CMD_TX_DATA
+	};
+
+	CAN_FORCE_CMD(&msg);
+	CAN_FORCE_COMPLETE(&msg);
+}
+
+void can_receive(int mob_id, int id, uint8_t *data, uint8_t length) {
 	st_cmd_t received_msg = {
 		.pt_data = &data[0],
 		.status = 0,
@@ -70,45 +55,34 @@ void can_receive(int mob_id, int id, uint8_t *data, uint8_t length){
 	mob_create(mob_id, &received_msg);
 }
 
-void can_testReceiver(void){
-	uint8_t received_data[TEST_MSG_LEN] = {0};
-	uint8_t received_data_other[6] = {0};
-	can_receive(10, 5, &received_data_other[0], 6);
-	can_receive(4, 4, &received_data[0], TEST_MSG_LEN);
+void can_testReceiver(void) {
+	can_receive(10, 5, &msg_buff[0], 6);
+	can_receive(4, 4, &msg_buff[0], 5);
 }
 
-void can_testSender(void){
-	uint8_t databuffer[TEST_MSG_LEN] = {'H', 'E', 'L', 'L', 'O'};
-	can_send(TEST_MSG_ID, &databuffer[0], TEST_MSG_LEN);
+void can_testSender(void) {
+	uint8_t databuffer[5] = {'H', 'E', 'L', 'L', 'O'};
+	can_send(4, &databuffer[0], 5);
 }
-
-/* Interrupt routine to take care of can interrupts */
-char str[64] = {0};
-uint8_t buffer[9] = {0};
 
 ISR (CANIT_vect) {
 	const uint16_t cansit = CANSIT2 + (CANSIT1 << 8); /* CAN Status Interrupt MOb Registers */
+	int i;
 
 	// Loop over each MOB and check if it have pending interrupt
-	int i;
 	for (i = 0; i <= LAST_MOB_NB; i++) {
 		if (BITMASK_CHECK(cansit, (0x01 << i))) { /* True if mob have pending interrupt */
 			Can_set_mob(i); /* Switch to mob */
 
 			switch (CANSTMOB) {
-				//case MOB_RX_COMPLETED_DLCW:
+				case MOB_RX_COMPLETED_DLCW:
 				case MOB_RX_COMPLETED:
-					//can_receive(TEST_MSG_ID, &buffer[0], TEST_MSG_LEN);
-					can_get_data(&buffer[0]);	// Copy data to canDataTest
-					//Can_mob_abort();        // Freed the MOB
+					can_get_data(&msg_buff[0]);	// Copy data to canDataTest
 
-					//can_receive(TEST_MSG_ID, &buffer[0], TEST_MSG_LEN); // put this before can_get_data and see if it works
-					//uart_txarr(UART_NUMBER_1, &buffer[0], TEST_MSG_LEN);
-					uart_printf(UART_NUMBER_1, "MOB_%d msg:%s\r\n", i, buffer);
-					memset(buffer, 0, 9);
+					uart_printf(UART_NUMBER_1, "MOB_%d msg:%s\r\n", i, msg_buff);
 
 					Can_clear_status_mob(); // and reset MOb status
-					BIT_SET(CANCDMOB, CONMOB1); /* enable reception. Need a define for this*/
+					BIT_SET(CANCDMOB, CONMOB1); /* enable reception */
 					break;
 				case MOB_TX_COMPLETED:
 					Can_mob_abort();        // Freed the MOB
